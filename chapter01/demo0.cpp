efb0a38f26e975f8f8a8d790ee55324356b10b3b
helisi233 <hls12343@163.com>
2022-08-14 09:05:37 +0800

    first commit

---
+++ chapter01/demo0.cpp
@@ -0,0 +1,22 @@
+//
+// Created by helisi on 2022/8/14.
+//
+#include <iostream>
+
+#define PI1 3.1415926
+
+int main(void)
+{
+    char c1 = 'a';
+    char a[10] = "a";
+    short int s = 97;
+    int m = 97;
+    long int n = 97;
+    float f = 97.0f;
+    double d = 97.0;
+    long double k = 97.0;
+    bool b = true;
+    wchar_t w[10] = L"a";
+
+    const double PI2 = 3.1415926;
+}
---
+++ cmake-build-debug/CMakeFiles/study_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/study_cpp.dir/chapter01/demo0.cpp.obj"
+  "study_cpp.pdb"
+  "study_cpp.exe"
+  "study_cpp.exe.manifest"
+  "libstudy_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/study_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
